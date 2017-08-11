4142522c94187773a04a61cf88b80042033ad84d
Matt DuLeone <matt@duleone.com>
2017-08-11 17:15:44 -0400

    favicon support

--- server/routes/app.js
+++ server/routes/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const favicon = require('serve-favicon');
 
 function appRoutes(app) {
     const routes = new express.Router();
@@ -13,10 +14,12 @@ function appRoutes(app) {
     if (production) {
         app.use('/static', express.static(path.join(__dirname, '../../static')));
         app.use('/', express.static(path.join(__dirname, '../../static')));
+        app.use(favicon(path.join(__dirname, '../../static', 'favicon.ico')));
     } else {
         // Currently, whether running this in production or not, it serves the most recent statically from /build. It is not meant to act as developmemt server.
         app.use('/static', express.static(path.join(__dirname, '../../build/static')));
         app.use('/', express.static(path.join(__dirname, '../../build/static')));
+        app.use(favicon(path.join(__dirname, '../../build/static', 'favicon.ico')));
     }
 
     routes.get('/*', (req, res) => {

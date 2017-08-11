d97481c7b09dabd1b51c6852f1c872dfde52d62c
Matt DuLeone <matt@duleone.com>
2017-08-11 16:50:11 -0400

    make api protocol agnostic

--- src/api/todo.js
+++ src/api/todo.js
@@ -1,7 +1,7 @@
 import {get} from './common';
 
 export function getGiphy(text) {
-    return get(`http://api.giphy.com/v1/gifs/translate?s=${encodeURIComponent(text)}&api_key=dc6zaTOxFJmzC`)
+    return get(`//api.giphy.com/v1/gifs/translate?s=${encodeURIComponent(text)}&api_key=dc6zaTOxFJmzC`)
         .then(response => {
             return response.data.images['fixed_height'].url;
         });
